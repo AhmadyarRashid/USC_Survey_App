4066d05e615aa5313a095d505abf9bbf6468758f
AhmadyarRashid <ahmadyarmeo@gmail.com>
2021-07-25 15:56:47 +0500

    fixed issue filter

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -27,6 +27,7 @@ function Dashboard(props) {
   const [company, setCompany] = useState("ptcl")
   const [userId, setUserId] = useState("-1")
   const [selectedStoreId, setStoreId] = useState("-1")
+  const [isLoading, setLoading] = useState(false)
 
   // selected area ids
   const [selectedZoneId, setSelectedZoneId] = useState("-1")
@@ -39,8 +40,10 @@ function Dashboard(props) {
       if (userInfo) {
         userInfo = JSON.parse(userInfo)
         setUserId(userInfo.userId)
+        setLoading(true)
         profileAPI(userInfo.userId)
           .then(response => {
+            setLoading(false)
             const {isSuccess = false, payload = {}, message = ''} = response
             if (isSuccess) {
               const {
@@ -80,14 +83,14 @@ function Dashboard(props) {
   const onChangeZoneId = async zoneId => {
     await setSelectedZoneId(zoneId)
     await setRegions(allRegions.filter(region => region.pid == zoneId))
-    await setCities(allCities.filter(city => city.pid == regionsDetail[0].id))
-    await setStores(allStores.filter(store => store.pid == citiesDetail[0].id))
+    await setCities(allCities.filter(city => city.pid == regions[0].id))
+    await setStores(allStores.filter(store => store.pid == cities[0].id))
   }
 
   const onChangeRegionId = async regionId => {
     await setSelectedRegionId(regionId)
     await setCities(allCities.filter(city => city.pid == regionId))
-    await setStores(allStores.filter(store => store.pid == citiesDetail[0].id))
+    await setStores(allStores.filter(store => store.pid == cities[0].id))
   }
 
   const onChangeCityId = async cityId => {
@@ -98,56 +101,59 @@ function Dashboard(props) {
   return (
     <Fragment style={{backgroundColor: 'white'}}>
       <HeaderComponent {...props}/>
-      <Container style={styles.root}>
-        <Content>
-          <Form>
-            <DropDown
-              name={area.headOffice}
-              list={headOffices}
-            />
-            <DropDown
-              name={area.zone}
-              list={zones}
-              onChangeOption={zoneId => onChangeZoneId(zoneId)}
-            />
-            <DropDown
-              name={area.region}
-              list={regions}
-              onChangeOption={regionId => onChangeRegionId(regionId)}
-            />
-            <DropDown
-              name={area.city}
-              list={cities}
-              onChangeOption={cityId => onChangeCityId(cityId)}
-            />
-            <DropDown
-              name={area.store}
-              list={stores}
-              onChangeOption={storeId => setStoreId(storeId)}
-            />
+      {isLoading
+        ? <Text style={{textAlign: 'center'}}>Loading...</Text>
+        : <Container style={styles.root}>
+          <Content>
+            <Form>
+              <DropDown
+                name={area.headOffice}
+                list={headOffices}
+              />
+              <DropDown
+                name={area.zone}
+                list={zones}
+                onChangeOption={zoneId => onChangeZoneId(zoneId)}
+              />
+              <DropDown
+                name={area.region}
+                list={regions}
+                onChangeOption={regionId => onChangeRegionId(regionId)}
+              />
+              <DropDown
+                name={area.city}
+                list={cities}
+                onChangeOption={cityId => onChangeCityId(cityId)}
+              />
+              <DropDown
+                name={area.store}
+                list={stores}
+                onChangeOption={storeId => setStoreId(storeId)}
+              />
 
-            <SingleOption
-              selectedOption={company}
-              setOption={setCompany}
-            />
+              <SingleOption
+                selectedOption={company}
+                setOption={setCompany}
+              />
 
-            <View style={{marginTop: 20}}>
-              <Button
-                onPress={() => props.navigation.navigate("Review", {
-                  company,
-                  userId,
-                  storeId: selectedStoreId
-                })}
-                block
-                success
-              >
-                <Text>Start</Text>
-                <Icon name='arrow-forward'/>
-              </Button>
-            </View>
-          </Form>
-        </Content>
-      </Container>
+              <View style={{marginTop: 20}}>
+                <Button
+                  onPress={() => props.navigation.navigate("Review", {
+                    company,
+                    userId,
+                    storeId: selectedStoreId
+                  })}
+                  block
+                  success
+                >
+                  <Text>Start</Text>
+                  <Icon name='arrow-forward'/>
+                </Button>
+              </View>
+            </Form>
+          </Content>
+        </Container>
+      }
     </Fragment>
   );
 }
